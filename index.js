31f9492498d3c5e29da7459ff20ac1796bac747f
Dean Rather <deanrather@gmail.com>
2016-07-05 14:21:07 +1000

    wip...

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 var express = require('express');
+
+
+
+/* eslint-disable no-console */
+var path = require('path');
+
+var GitKit = require('gitkit');
+var NodeFS = require('./node_modules/gitkit/lib/fs/node');
+
+// Create an FS to write the output repository
+var fs = new NodeFS(path.resolve(__dirname, 'data'));
+
+// Create a repository instance
+var repo = GitKit.Repository.createWithFS(fs);
+
+// Initialize the repository
+GitKit.RepoUtils.init(repo)
